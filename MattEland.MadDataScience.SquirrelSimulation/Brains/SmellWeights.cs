113f24b3f3532994a9799ee76f404a50f2f3b81d
Matt Eland <Matt.Eland@GMail.com>
2024-12-31 23:08:24 -0500

    Fixed binding issue on sliders.

--- MattEland.MadDataScience.SquirrelSimulation/Brains/SmellWeights.cs
+++ MattEland.MadDataScience.SquirrelSimulation/Brains/SmellWeights.cs
@@ -1,10 +1,13 @@
 namespace MattEland.MadDataScience.SquirrelSimulation.Brains;
 
-public class SmellWeights
+public record SmellWeights
 {
     public float Acorn { get; set; }
     public float Squirrel { get; set; }
     public float Doggo { get; set; }
     public float Rabbit { get; set; }
     public float Tree { get; set; }
+
+    public override string ToString() 
+        => $"Acorn: {Acorn}, Squirrel: {Squirrel}, Doggo: {Doggo}, Rabbit: {Rabbit}, Tree: {Tree}";
 }

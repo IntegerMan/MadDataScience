8b9fc3037bcda5939b3e7bd48f22d8f73e89fad0
Matt Eland <Matt.Eland@GMail.com>
2024-12-31 21:47:32 -0500

    Weighted brain implemented, but not working properly

---
+++ MattEland.MadDataScience.SquirrelSimulation/Brains/SmellWeights.cs
@@ -0,0 +1,10 @@
+namespace MattEland.MadDataScience.SquirrelSimulation.Brains;
+
+public class SmellWeights
+{
+    public float Acorn { get; set; }
+    public float Squirrel { get; set; }
+    public float Doggo { get; set; }
+    public float Rabbit { get; set; }
+    public float Tree { get; set; }
+}
--- MattEland.MadDataScience.SquirrelSimulation/Brains/WeightedBrain.cs
+++ MattEland.MadDataScience.SquirrelSimulation/Brains/WeightedBrain.cs
@@ -2,10 +2,29 @@ namespace MattEland.MadDataScience.SquirrelSimulation.Brains;
 
 public class WeightedBrain : IBrain
 {
+    public SmellWeights Weights { get; init; } = new();
+    
     public WorldPosition GetGameMove(IGameActor actor, IEnumerable<TilePerceptions> choices, Random random)
     {
-        // TODO: This will need to take in some tile importances and use those for its decisions
+        float bestScore = float.MinValue;
+        WorldPosition? bestPosition = null;
         
-        return actor.Position;
+        // Random order here helps in cases of tie scores since the first with that value will win
+        foreach (TilePerceptions choice in choices.OrderBy(_ => random.Next()))
+        {
+            float score = (choice.SmellOfSquirrel * Weights.Squirrel) + 
+                          (choice.SmellOfRabbit * Weights.Rabbit) +
+                          (choice.SmellOfDoggo * Weights.Doggo) +
+                          (choice.SmellOfTree * Weights.Tree) +
+                          (choice.SmellOfAcorn * Weights.Acorn);
+            
+            if (score > bestScore)
+            {
+                bestScore = score;
+                bestPosition = choice.Position;
+            }
+        }
+        
+        return bestPosition ?? actor.Position;
     }
 }
